8bc9bb721dda93433763f7b4a5ad144936f09b44
Mehul Poddar <mehulpoddar1310@gmail.com>
2023-02-06 20:04:53 +0530

    data collection

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 11:47:44 2020
+
+@author: Ken
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/Mehul/Desktop/ds_salary_proj/chromedriver.exe"
+df = gs.get_jobs('data scientist',10, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
