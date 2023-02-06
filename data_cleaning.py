7bdfd65c485db4c40ca5920c881aafad954ba107
Mehul Poddar <mehulpoddar1310@gmail.com>
2023-02-06 20:05:40 +0530

    data cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,69 @@
+import pandas as pd
+df  = pd.read_csv("C:/Users/Mehul/Desktop/ds_salary_proj/glassdoor_jobs.csv")
+
+#salary parsing
+#company name text only
+#state field
+#age of company
+#parsing of job description(python, etc.)
+
+
+#salary parsing
+
+df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+
+df = df[df['Salary Estimate'] != '-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+
+min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+
+df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+df['avg_salary'] = (df.min_salary+df.max_salary)/2
+
+
+#company name text only
+df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating']<0 else x['Company Name'][:-3], axis=1)
+
+#state field
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+print(df.job_state.value_counts())
+
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis =1)
+
+#age of the company
+df['age'] = df.Founded.apply(lambda x: x if x <1 else 2021-x)
+
+#parsing of job description(python, etc.)
+
+#python
+df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+print(df.python_yn.value_counts())
+
+#r studio
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r studio' in x.lower() else 0)
+print(df.R_yn.value_counts())
+
+#spark
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+print(df.spark.value_counts())
+
+#aws
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+print(df.aws.value_counts())
+
+#excel
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+print(df.excel.value_counts())
+
+df.columns
+
+df_out = df.drop(['Unnamed: 0'], axis =1)
+
+df_out.to_csv('salary_data_cleaned.csv',index = False)
+
+
+
+
